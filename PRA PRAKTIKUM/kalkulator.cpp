7d3331fa7b2d8c269fdd727eb6679708fb7d5562
rajuputra <raju2100018316@webmail.uad.ac.id>
2022-03-18 20:25:56 +0700

    Update ke-2 kalkulator-cpp

--- PRA PRAKTIKUM/kalkulator.cpp
+++ PRA PRAKTIKUM/kalkulator.cpp
@@ -36,6 +36,32 @@ int main(){
     cout << endl ;
     cout << " Masukkan pilihan anda : ";
     cin >> pilihan;
-   
+
+// Raju Putra
+    //Raju Putra Dermawan
+    switch (pilihan){
+    	case 1 :
+    		cout << " Angka pertama yang anda pilih "<<a;
+    		cout << " \n Angka kedua yang anda pilih "<<b;
+    		cout << " \n Hasil " << a <<" Tambah " << b << " = "<< tambah(a,b);
+    		break;
+    	case 2 :
+    		cout << " Angka pertama yang anda pilih "<<a;
+    		cout << " \n Angka kedua yang anda pilih "<<b;
+    		cout << " \n Hasil " << a <<" kurang " << b << " = "<< kurang(a,b);
+    		break;
+    	case 3 :
+    		cout << " Angka pertama yang anda pilih "<<a;
+    		cout << " \n Angka kedua yang anda pilih "<<b;
+    		cout << " \n Hasil " << a <<" Bagi " << b << " = "<< bagi(a,b);
+    		break;
+    	case 4 :
+    		cout << " Angka pertama yang anda pilih "<<a;
+    		cout << " \n Angka kedua yang anda pilih "<<b;
+    		cout << " \n Hasil " << a <<" Kali " << b << " = "<< kali(a,b);
+    		break;
+	}
+	cout << endl;
+	goto x;
     return 0;
 }
