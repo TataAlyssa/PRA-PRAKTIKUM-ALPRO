0ac094e224799a0e984a374d9784ebf228438b96
imhelium <imhelium02@gmail.com>
2022-03-18 16:44:47 +0700

    membuat kalkulator-cpp

---
+++ PRA PRAKTIKUM/kalkulator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// Tata Alyssa 
+int tambah (int a, int b){
+    int hasil = a+b;
+    return hasil ;
+} 
+
+int kurang (int a, int b){
+	int hasil = a-b;
+	return hasil;
+}
+
+int bagi (int a, int b){
+	int hasil = a/b;
+	return hasil;
+}
+
+int kali (int a, int b){
+	int hasil = a*b;
+	return hasil;
+}
+
