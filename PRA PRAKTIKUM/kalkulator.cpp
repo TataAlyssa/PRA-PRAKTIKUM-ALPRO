29178a9eb42ff8980aee9157be4f9c8add17ffb3
Khulfani <khulfani1@gmail.com>
2022-03-18 20:09:44 +0700

    Update 1 kakulator-cpp

--- PRA PRAKTIKUM/kalkulator.cpp
+++ PRA PRAKTIKUM/kalkulator.cpp
@@ -20,4 +20,22 @@ int kali (int a, int b){
 	int hasil = a*b;
 	return hasil;
 }
-
+// khulfani
+int main(){
+    int a,b,pilihan;
+    x:
+    cout << " Program Kalkulator Sederhana \n";
+    cout << endl;
+    cout << " Silahkan Masukkan angka pertama : ";
+    cin >> a ;
+    cout << " Silahkan Masukkan angka kedua : ";
+    cin >> b ;
+    cout << endl ;
+    cout << " Menu Operasi: " << endl;
+    cout << "1. Tambah \n2. Kurang \n3. Bagi \n4. Kali \n";
+    cout << endl ;
+    cout << " Masukkan pilihan anda : ";
+    cin >> pilihan;
+   
+    return 0;
+}
